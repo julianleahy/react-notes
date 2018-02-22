8c40d3f1f6b28563f0adc186cf0452044b9e0213
julianleahy <julianleahynz@gmail.com>
2018-02-22 13:33:20 +1300

    change note to a presentational component and pass props from root

--- src/Note/Note.js
+++ src/Note/Note.js
@@ -1,18 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Note.css';
 
-class Note extends Component {
-
-  render() {
-    return(
-      <div className="Note">
-        <p className="NoteContent">
-          lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus autem rerum sed distinctio eos! Eum, molestias voluptatem! Recusandae, optio voluptatibus?
+const Note = (props) => (
+  <div className="Note">
+    <p className="NoteContent">
+      lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus autem rerum sed distinctio eos! Eum, molestias voluptatem! Recusandae, optio voluptatibus?
         </p>
-        <button className="NoteDelete">X</button>
-      </div>
-    );
-  }
-}
+    <button className="NoteDelete">X</button>
+  </div>
+);
 
 export default Note;
+
