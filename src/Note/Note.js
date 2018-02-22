a8f2ea65adfdef0c079af4d658b8238fc78a86d4
julianleahy <julianleahynz@gmail.com>
2018-02-22 13:21:22 +1300

    style note paragraph

--- src/Note/Note.js
+++ src/Note/Note.js
@@ -7,7 +7,7 @@ class Note extends Component {
     return(
       <div className="Note">
         <p className="NoteContent">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus autem rerum sed distinctio eos! Eum, molestias voluptatem! Recusandae, optio voluptatibus?
+          lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus autem rerum sed distinctio eos! Eum, molestias voluptatem! Recusandae, optio voluptatibus?
         </p>
         <button className="NoteDelete">X</button>
       </div>
